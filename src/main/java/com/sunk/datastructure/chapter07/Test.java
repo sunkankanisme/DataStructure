9618132a5f28ee30ba22ae48851950e9dffc18a6
sunkangkang <sunkangkang@focusmedia.cn>
2023-01-09 15:09:15 +0800

    增加时间复杂度测试代码

---
+++ src/main/java/com/sunk/datastructure/chapter07/Test.java
@@ -0,0 +1,20 @@
+package com.sunk.datastructure.chapter07;
+
+/**
+ * @author sunk
+ * @since 2023/1/9
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+
+        int n = 100;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+            }
+        }
+
+    }
+
+}
