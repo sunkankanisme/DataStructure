1b5efc7cb3ab2642400e4f62043a1f574f1caa1c
sunkangkang <sunkangkang@focusmedia.cn>
2023-02-01 14:16:40 +0800

    增加二叉树遍历、查找算法

---
+++ src/main/java/com/sunk/datastructure/chapter10/BinaryTree.java
@@ -0,0 +1,221 @@
+package com.sunk.datastructure.chapter10;
+
+import lombok.Data;
+
+/**
+ * @author sunk
+ * @since 2023/2/1
+ **/
+public class BinaryTree {
+
+    private HeroNode root;
+
+    public void setRoot(HeroNode root) {
+        this.root = root;
+    }
+
+    /*
+     * 前序遍历
+     */
+    public void preOrder() {
+        if (this.root != null) {
+            this.root.preOrder();
+        }
+    }
+
+    /*
+     * 中序遍历
+     */
+    public void infixOrder() {
+        if (this.root != null) {
+            this.root.infixOrder();
+        }
+    }
+
+    /*
+     * 后序遍历
+     */
+    public void postOrder() {
+        if (this.root != null) {
+            this.root.postOrder();
+        }
+    }
+
+    public HeroNode preOrderSearch(int no) {
+        if (root != null) {
+            return root.preOrderSearch(no);
+        }
+
+        return null;
+    }
+
+    public HeroNode infixOrderSearch(int no) {
+        if (root != null) {
+            return root.infixOrderSearch(no);
+        }
+
+        return null;
+    }
+
+    public HeroNode postOrderSearch(int no) {
+        if (root != null) {
+            return root.postOrderSearch(no);
+        }
+
+        return null;
+    }
+
+
+    /*
+     * 创建节点对象
+     */
+    @Data
+    static class HeroNode {
+        private int no;
+        private String name;
+        private HeroNode left;
+        private HeroNode right;
+
+        public HeroNode(int no, String name) {
+            this.no = no;
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "HeroNode{" +
+                    "no=" + no +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+
+        /*
+         * 前序遍历, 父左右
+         */
+        public void preOrder() {
+            System.out.println(this);
+
+            if (this.left != null) {
+                this.left.preOrder();
+            }
+
+            if (this.right != null) {
+                this.right.preOrder();
+            }
+        }
+
+        /*
+         * 中序遍历, 左父右
+         */
+        public void infixOrder() {
+            if (this.left != null) {
+                this.left.preOrder();
+            }
+
+            System.out.println(this);
+
+            if (this.right != null) {
+                this.right.preOrder();
+            }
+        }
+
+        /*
+         * 后序遍历, 左右父
+         */
+        public void postOrder() {
+            if (this.left != null) {
+                this.left.preOrder();
+            }
+
+            if (this.right != null) {
+                this.right.preOrder();
+            }
+
+            System.out.println(this);
+        }
+
+        /*
+         * 前序遍历查找
+         */
+        public HeroNode preOrderSearch(int no) {
+            System.out.println("preOrderSearch");
+            // 判断当前节点
+            if (this.no == no) {
+                return this;
+            }
+
+            // 左递归查找
+            HeroNode tmpNode = null;
+            if (this.left != null) {
+                tmpNode = this.left.preOrderSearch(no);
+
+                if (tmpNode != null) {
+                    return tmpNode;
+                }
+            }
+
+            // 右递归查找
+            if (this.right != null) {
+                tmpNode = this.right.preOrderSearch(no);
+            }
+
+            return tmpNode;
+        }
+
+        /*
+         * 中序遍历查找
+         */
+        public HeroNode infixOrderSearch(int no) {
+            HeroNode tmpNode = null;
+            if (this.left != null) {
+                tmpNode = this.left.infixOrderSearch(no);
+
+                if (tmpNode != null) {
+                    return tmpNode;
+                }
+            }
+
+            System.out.println("infixOrderSearch");
+            if (this.no == no) {
+                return this;
+            }
+
+            if (this.right != null) {
+                tmpNode = this.right.infixOrderSearch(no);
+            }
+
+            return tmpNode;
+        }
+
+        /*
+         * 后序遍历查找
+         */
+        public HeroNode postOrderSearch(int no) {
+            HeroNode tmpNode = null;
+            if (this.left != null) {
+                tmpNode = this.left.postOrderSearch(no);
+
+                if (tmpNode != null) {
+                    return tmpNode;
+                }
+            }
+
+            if (this.right != null) {
+                tmpNode = this.right.postOrderSearch(no);
+
+                if (tmpNode != null) {
+                    return tmpNode;
+                }
+            }
+
+            System.out.println("postOrderSearch");
+            if (this.no == no) {
+                return this;
+            }
+
+            return null;
+        }
+
+    }
+
+}
---
+++ src/main/java/com/sunk/datastructure/chapter10/Main.java
@@ -0,0 +1,51 @@
+package com.sunk.datastructure.chapter10;
+
+/**
+ * @author sunk
+ * @since 2023/2/1
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        // 创建二叉树
+        final BinaryTree tree = new BinaryTree();
+
+        // 创建节点
+        final BinaryTree.HeroNode node1 = new BinaryTree.HeroNode(1, "宋江");
+        final BinaryTree.HeroNode node2 = new BinaryTree.HeroNode(2, "吴用");
+        final BinaryTree.HeroNode node3 = new BinaryTree.HeroNode(3, "卢俊义");
+        final BinaryTree.HeroNode node4 = new BinaryTree.HeroNode(4, "林冲");
+
+        /*
+         * 挂载节点
+         *
+         *          node1(root)
+         *      node2       node3
+         *     -     -     -    node4
+         */
+        tree.setRoot(node1);
+        node1.setLeft(node2);
+        node1.setRight(node3);
+        node3.setRight(node4);
+
+        // 测试
+        System.out.println("====== 前序遍历");
+        tree.preOrder();                // 1,2,3,4
+        System.out.println("====== 中序遍历");
+        tree.infixOrder();              // 2,1,3,4
+        System.out.println("====== 后序遍历");
+        tree.postOrder();               // 2,3,4,1
+
+        // 查找
+        final BinaryTree.HeroNode node5 = new BinaryTree.HeroNode(5, "关胜");
+        node3.setLeft(node5);
+        System.out.println("====== 前序遍历查找");
+        System.out.println(tree.preOrderSearch(5));
+        System.out.println("====== 中序遍历查找");
+        System.out.println(tree.infixOrderSearch(5));
+        System.out.println("====== 后序遍历查找");
+        System.out.println(tree.postOrderSearch(5));
+
+    }
+
+}
