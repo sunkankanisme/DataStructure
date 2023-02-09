b39a29343e6c6372a886c3427b8072ee4c17b5d1
sunk <sunkankanisme@163.com>
2023-02-09 15:27:07 +0800

    增加二叉排序树的删除代码

--- src/main/java/com/sunk/datastructure/chapter10/bst/BinarySortTree.java
+++ src/main/java/com/sunk/datastructure/chapter10/bst/BinarySortTree.java
@@ -29,6 +29,119 @@ public class BinarySortTree {
         root.infixOrder();
     }
 
+    /*
+     * 查找节点
+     */
+    public Node search(int value) {
+        if (root == null) {
+            return null;
+        }
+
+        return root.search(value);
+    }
+
+    /*
+     * 查找节点父节点
+     */
+    public Node searchParent(int value) {
+        if (root == null) {
+            return null;
+        }
+
+        return root.searchParent(value);
+    }
+
+    /*
+     * 删除节点
+     */
+    public void deleteNode(int value) {
+        if (root == null) {
+            return;
+        }
+
+        // 查找要删除的节点
+        final Node targetNode = search(value);
+        // 没有找到要删除的节点
+        if (targetNode == null) {
+            return;
+        }
+
+        // 查找要删除的节点的父节点
+        final Node parentNode = searchParent(value);
+        // 没有找到目标节点的父节点
+        if (parentNode == null) {
+            root = null;
+            return;
+        }
+
+        /*
+         * 第一种情况：要删除的节点是叶子节点
+         */
+        if (targetNode.left == null && targetNode.right == null) {
+            // 判断 targetNode 是 parentNode 的 左/右 子节点，并分别置空删除
+            if (parentNode.left != null && parentNode.left.value == targetNode.value) {
+                parentNode.left = null;
+            } else if (parentNode.right != null && parentNode.right.value == targetNode.value) {
+                parentNode.right = null;
+            }
+        }
+
+        /*
+         * 第三种情况：删除的节点有左右子树
+         */
+        else if (targetNode.left != null && targetNode.right != null) {
+            // 找到右子树的最小节点（或左子树的最大节点）
+            targetNode.value = deleteRightTreeMin(targetNode.right);
+        }
+
+        /*
+         * 第二种情况：删除的节点是单子树节点
+         */
+        else {
+            // 1 如果目标节点节点存在左子节点
+            if (targetNode.left != null) {
+                // 1.1 目标节点是其父节点的左子节点
+                if (parentNode.left.value == targetNode.value) {
+                    parentNode.left = targetNode.left;
+                }
+                // 1.2 目标节点是其父节点的右子节点
+                else {
+                    parentNode.right = targetNode.left;
+                }
+            }
+            // 2 如果目标节点节点存在右子节点
+            else {
+                // 2.1 目标节点是其父节点的左子节点
+                if (parentNode.left.value == targetNode.value) {
+                    parentNode.left = targetNode.right;
+                }
+                // 2.2 目标节点是其父节点的右子节点
+                else {
+                    parentNode.right = targetNode.right;
+                }
+            }
+        }
+    }
+
+    /**
+     * 从传入的二叉排序树中找到最小节点的值，同时删除最小的节点
+     *
+     * @param node 传入的节点
+     **/
+    private int deleteRightTreeMin(Node node) {
+        Node target = node;
+
+        // 循环的查找左节点，就会找到最小值
+        while (target.left != null) {
+            target = target.left;
+        }
+
+        // 这时 target 指向了最小值，删除最小的节点
+        deleteNode(target.value);
+
+        return target.value;
+    }
+
 
     /*
      * 内部节点类
@@ -82,6 +195,44 @@ public class BinarySortTree {
 
             if (this.right != null) {
                 this.right.infixOrder();
+            }
+        }
+
+
+        /*
+         * 查找指定节点
+         */
+        public Node search(int value) {
+            if (value == this.value) {
+                return this;
+            } else if (value < this.value && this.left != null) {
+                // 向左子树递归查找
+                return this.left.search(value);
+            } else if (value > this.value && this.right != null) {
+                // 向右子树递归查找
+                return this.right.search(value);
+            }
+
+            return null;
+        }
+
+        /*
+         * 查找指定节点父节点方法
+         */
+        public Node searchParent(int value) {
+            if ((this.left != null && this.left.value == value) || (this.right != null && this.right.value == value)) {
+                return this;
+            } else {
+                if (value < this.value && this.left != null) {
+                    // 如果查找的值小于当前节点的值，并且当前节点的左子节点不为空，向左子树递归查找
+                    return this.left.searchParent(value);
+                } else if (value >= this.value && this.right != null) {
+                    // 向右子树递归查找
+                    return this.right.searchParent(value);
+                } else {
+                    // 到此处表示找不到，要删除的节点没有父节点
+                    return null;
+                }
             }
         }
 
--- src/main/java/com/sunk/datastructure/chapter10/bst/BinarySortTreeTest.java
+++ src/main/java/com/sunk/datastructure/chapter10/bst/BinarySortTreeTest.java
@@ -3,7 +3,7 @@ package com.sunk.datastructure.chapter10.bst;
 public class BinarySortTreeTest {
 
     public static void main(String[] args) {
-        int[] arr = {7, 3, 10, 12, 5, 1, 9};
+        int[] arr = {7, 3, 10, 12, 5, 1, 9, 2};
         final BinarySortTree binarySortTree = new BinarySortTree();
 
         // 添加元素
@@ -14,6 +14,20 @@ public class BinarySortTreeTest {
         // 中序遍历
         binarySortTree.infixOrder();
 
+        // 测试删除叶子节点
+        // System.out.println("=== 测试删除叶子节点");
+        // binarySortTree.deleteNode(7);
+        // binarySortTree.infixOrder();
+
+        // 测试删除单子树节点
+        // System.out.println("=== 测试删除单子树节点");
+        // binarySortTree.deleteNode(1);
+        // binarySortTree.infixOrder();
+
+        // 测试删除双子树节点
+        System.out.println("=== 测试删除双子树节点");
+        binarySortTree.deleteNode(3);
+        binarySortTree.infixOrder();
 
     }
 
